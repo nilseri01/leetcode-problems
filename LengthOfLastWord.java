16dff17c6b15e0be74f4844ea5975da64d5529a7
Nil Seri <senoritadeveloper01@gmail.com>
2022-04-08 20:58:16 +0300

    Create LengthOfLastWord.java

---
+++ LengthOfLastWord.java
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/length-of-last-word/
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String[] words = s.split(" ");
+        return words[words.length - 1].length();
+    }
+}
