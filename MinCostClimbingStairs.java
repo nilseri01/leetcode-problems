c27fc9e5996bb930287a241da5f84a2b754ff030
Nil Seri <senoritadeveloper01@gmail.com>
2022-06-15 17:39:05 +0300

    Create MinCostClimbingStairs.java

---
+++ MinCostClimbingStairs.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/min-cost-climbing-stairs
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int dp[] = new int[n];
+        if (n == 1) {
+            return cost[0];
+        }
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+        }
+        
+        return Math.min(dp[n - 2], dp[n - 1]);
+    }
+}
