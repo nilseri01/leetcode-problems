3bfc4161ff1084f4b51475fce925a0edfaeaeee2
Nil Seri <senoritadeveloper01@gmail.com>
2022-06-18 19:22:46 +0300

    Create MaximumUnitsOnATruck.java

---
+++ MaximumUnitsOnATruck.java
@@ -0,0 +1,24 @@
+//
+// greedy solution from https://github.com/ShreyasKadiri/LeetCode/blob/master/Maximum%20Units%20on%20a%20Truck.java
+
+class Solution {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        int [] unitCount = new int[1001];
+        int count = 0;
+        for (int[] boxType : boxTypes){
+            unitCount[boxType[1]] += boxType[0];
+        }
+        for (int i=unitCount.length-1; i>=0; i--){
+            if (unitCount[i] == 0){
+                continue;
+            }
+            int num = Math.min(unitCount[i], truckSize);
+            count += num * i;
+            truckSize -= num;
+            if (truckSize == 0){
+                break;
+            }
+        }
+        return count;
+    }
+}
